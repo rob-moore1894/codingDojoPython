ff670e5ef6426dd3c73e44dfe44d16ef1b306f10
Rob Moore <moore.re@gmail.com>
2020-08-03 14:39:15 -0500

    Completed first python program

---
+++ helloworld/hello.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+name = "Rob"
+print("Hello", name)
+print("Hello " + name)
+
+num = 42
+print ("Hello", num)
+print ("Hello " + str(num))
+
+food1 = "tacos"
+food2 = "sushi"
+print("I love to eat {} and {}" .format(food1, food2))
+print(f"I love to eat {food1} and {food2}")
+
+#NINJA BONUS
+print(name)
+print (name.title())
+print("I could live off of %s and %s" % (food1, food2))
