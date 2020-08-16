230d1b5b39af84d2f898c4d3b94985c6334c04b5
Rob Moore <moore.re@gmail.com>
2020-08-16 17:33:44 -0500

    Completed First Django Project Exercise

--- django/django_intro/firstDjangoProject/djangoApp/views.py
+++ django/django_intro/firstDjangoProject/djangoApp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
 
 # Create your views here.
+def root(request):
+    return redirect("/blogs")
+
 def index(request):
-    return HttpResponse("This is the equivalent of @app.route('/')!")
+    return HttpResponse("Placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("Placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request, number):
+    return HttpResponse(f"Placeholder to display blog number {number}")
+
+def edit(request, number):
+    return HttpResponse(f"Placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect('/blogs')
+
+def json(request):
+    blog = {
+        'title': 'Blog title',
+        'content': 'Blog content'
+    }
+    return JsonResponse(blog)
