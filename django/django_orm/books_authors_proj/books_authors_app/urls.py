2627bc3cfdbbfdcb0ab1f5bc93ba5c3403f84f7d
Rob Moore <moore.re@gmail.com>
2020-08-20 16:09:19 -0500

    Completed Books and Authors with Templates

--- django/django_orm/books_authors_proj/books_authors_app/urls.py
+++ django/django_orm/books_authors_proj/books_authors_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index),
     path('addBook', views.addBook),
     path('books/<int:bookId>', views.showBook),
+    path('addAuthorToBook/<int:bookId>', views.addAuthorToBook),
     path('authors', views.authors),
+    path('addAuthor', views.addAuthor),
+    path('authors/<int:authorId>', views.showAuthor),
+    path('addBookToAuthor/<int:authorId', views.addBookToAuthor),
 ]
--- django/django_orm/books_authors_proj/books_authors_app/views.py
+++ django/django_orm/books_authors_proj/books_authors_app/views.py
@@ -19,12 +19,42 @@ def showBook(request, bookId):
     context = {
         'identifiedBook': bookToShow,
         'all_authors': Author.objects.all(),
-        'identifiedAuthors': bookAuthors
+        'otherAuthors': bookAuthors
     }
     return render(request, "book.html", context)
+
+def addAuthorToBook(request, bookId):
+    print(request.POST) #I get the id of the author
+    this_author = Author.objects.get(id = request.POST['authorInfo'])
+    this_book = Book.objects.get(id=bookId)
+
+    this_book.authors.add(this_author)
+    return redirect(f'/books/{bookId}')
 
 def authors(request):
     context = {
         "all_authors": Author.objects.all()
     }
     return render(request, "authors.html", context)
+
+def addAuthor(request):
+    newAuthor = Author.objects.create(first_name= request.POST['first_name'], last_name=request.POST['last_name'], notes=request.POST['notes'])
+    newAuthor.save()
+    return redirect('/authors')
+
+def showAuthor(request, authorId):
+    authorToShow = Author.objects.get(id = authorId)
+    authorBook = Book.objects.exclude(id = authorToShow.id)
+    context = {
+        'identifiedAuthor': authorToShow,
+        'all_books': Book.objects.all(),
+        'otherBooks': authorBook
+    }
+    return render(request,"author.html", context)
+
+def addBookToAuthor(request, authorId):
+    this_book = Book.objects.get(id = request.POST['bookInfo'])
+    this_author = Author.objects.get(id = authorId)
+
+    this_author.book.add(this_book)
+    return redirect(f'/author/{authorId}')
