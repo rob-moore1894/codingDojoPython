5ee24b6d756c7df381ed85ba006e56ed923efdfb
Rob Moore <moore.re@gmail.com>
2020-08-20 12:03:21 -0500

    Completed Morning Algos Aug 20

--- algos/stack.py
+++ algos/stack.py
@@ -3,6 +3,58 @@ class Node:
         self.value = value
         self.next = None
 
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, value):
+        #create a new node
+        newNode = Node(value)
+        #if queue is empty, set self.head and self.tail to value
+        if self.head == None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = self.tail.next
+        return self
+
+    #dequeue means to remove from the head of the queue
+    def dequeue(self):
+        if self.head == None:
+            print("Nothing to remove")
+        else:
+            self.head = self.head.next
+            if self.head == None:
+                self.tail = None
+        return self
+
+    def size(self):
+        runner = self.head
+        count = 0
+        while runner != None:
+            count += 1
+            runner = runner.next
+        print(count)
+        return self
+
+    def display(self):
+        runner = self.head
+        while runner != None:
+            print(runner.value)
+            runner = runner.next
+        print(runner)
+        return self
+
+    def contains(self, val): 
+        runner = self.head
+        while runner != None:
+            if runner.value == val:
+                return True
+            runner = runner.next
+        return False
+
 class Stack:
     def __init__(self):
         self.top = None
@@ -45,3 +97,28 @@ class Stack:
 stk1 = Stack()
 stk1.push(6).push(1).push(9).display()
 stk1.display()
+
+q1 = Queue()
+
+q1.enqueue('h').enqueue('a').enqueue('n').enqueue('n').enqueue('a').enqueue('h')
+
+
+def palindrome(queueInput):
+    stk = Stack()
+    length = queueInput.size()
+    for i in range(length):
+        t = queueInput.dequeue()
+        queueInput.enqueue(t)
+        stk.push(t)
+    rnr = queueInput.head
+    rnr2 = stk.top
+    while rnr is not None:
+        if rnr.value is not rnr2.value:
+            print(False)
+            return False
+        rnr = rnr.next
+        rnr2 = rnr2.next
+    print(True)
+
+
+palindrome(q1)
