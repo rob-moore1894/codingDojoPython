032dccd80ca4d87a4391fca31f2687545acc6771
Rob Moore <moore.re@gmail.com>
2020-08-20 23:45:10 -0500

    Completed SemiRESTful TV Shows Validated Assignment

--- django/django_full_stack/tv_proj/tv_app/models.py
+++ django/django_full_stack/tv_proj/tv_app/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class showsManager(models.Manager):
+    def showValidator(self, postData):
+        errors = {}
+        if len(postData['inputShowTitle']) < 2:
+            errors['titleReq'] = "Title must be at least 2 characters"
+        if len(postData['inputShowNetwork']) < 3:
+            errors['networkReq'] = "Network must be at least 3 characters"
+        if len(postData['inputReleaseDate']) == 0:
+            errors['releaseDateReq'] = "Release Date Required"
+        if len(postData['inputShowDesc']) > 0 and len(postData['inputShowDesc']) < 10:
+            errors['showDescReq'] = "Description must be at least 10 characters"
+        return errors
+
+
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
     releaseDate = models.CharField(max_length=255)
     description = models.TextField(default="")
+    objects = showsManager()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- django/django_full_stack/tv_proj/tv_app/views.py
+++ django/django_full_stack/tv_proj/tv_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, render_to_response
-from django.template import RequestContext
+from django.shortcuts import render, redirect
 from .models import Shows
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,11 @@ def newShowPage(request):
 
 def createShow(request):
     print(request.POST)
+    errorsFound = Shows.objects.showValidator(request.POST)
+    if len(errorsFound) > 0:
+        for value in errorsFound.values():
+            messages.error(request, value)
+        return redirect("/shows/new")
     newShow = Shows.objects.create(title = request.POST['inputShowTitle'], network = request.POST['inputShowNetwork'], releaseDate= request.POST['inputReleaseDate'], description=request.POST['inputShowDesc'])
     newShow.save()
     return redirect(f"/shows/{newShow.id}")
@@ -35,14 +41,15 @@ def editShowPage(request, showId):
     
 def editShow(request, showId):
     this_show = Shows.objects.get(id=showId)
-    if len(request.POST['inputShowTitle']) > 0:
-        this_show.title = request.POST['inputShowTitle']
-    if len(request.POST['inputShowNetwork']) > 0:
-        this_show.network = request.POST['inputShowNetwork']
-    if len(request.POST['inputReleaseDate']) > 0:
-        this_show.releaseDate = request.POST['inputReleaseDate']
-    if len(request.POST['inputShowDesc']) > 0:
-        this_show.description = request.POST['inputShowDesc']
+    errorsFound = Shows.objects.showValidator(request.POST)
+    if len(errorsFound) > 0:
+        for value in errorsFound.values():
+            messages.error(request, value)
+        return redirect(f"/shows/{showId}/edit") 
+    this_show.title = request.POST['inputShowTitle']
+    this_show.network = request.POST['inputShowNetwork']
+    this_show.releaseDate = request.POST['inputReleaseDate']
+    this_show.description = request.POST['inputShowDesc']
     this_show.save()
     return redirect(f"/shows/{showId}")
 
@@ -50,8 +57,3 @@ def destroy(request, showId):
     this_show = Shows.objects.get(id=showId)
     this_show.delete()
     return redirect("/shows")
-
-def handler404(request):
-    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
